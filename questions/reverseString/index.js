80953c692f4b6359075a3e6042e9878f1390839c
Joe <josephlewismoulton@gmail.com>
2023-01-05 16:00:13 +0000

    Reverse String completed

--- questions/reverseString/index.js
+++ questions/reverseString/index.js
@@ -14,7 +14,25 @@
 // a = 'ivaD', c = 'd' => a = 'd' + 'ivaD' = 'divaD'
 
 const reverseString = (str) => {
+  //     let reversed = "";
 
+  //     for (let i = 0; i < str.length; ++i) {
+  //       reversed = str[i] + reversed;
+  //     }
+
+  //     return reversed
+  //     Reversed string by iterating through and adding to itself
+
+  //     ¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬
+
+  //    return str.split("").reverse().join("");
+  //    converts to array and reverses it and joins it back together
+
+  //     ¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬
+
+  return str
+    .split("")
+    .reduce((accumulator, currentValue) => currentValue + accumulator);
 };
 
 module.exports = reverseString;
